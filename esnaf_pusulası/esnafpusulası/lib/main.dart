3cef8556c0eec77068773a96e830212df6147126
Tuba <114258460+tubaunlu@users.noreply.github.com>
2025-07-14 23:30:54 +0300

    Update main.dart

--- esnaf_pusulası/esnafpusulası/lib/main.dart
+++ esnaf_pusulası/esnafpusulası/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Esnaf Pusulası',
-      // Giriş ekranını ana ekran olarak ayarladık
+
       home: const LoginScreen(),
     );
   }
